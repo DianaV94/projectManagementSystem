745ccb932fec2f8f963b6f096aeb0a2bdf807094
Diana <diana@Puscas.local>
2020-05-07 20:33:18 +0300

    Refactor code
    - Move code outside of graphql types definitions.
    - Disable file upload as it is not support pre NodeJs 8.15.0

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -8,7 +8,7 @@ const typeDefs = gql`
 
   # This "Book" type defines the queryable fields for every book in our data source.
   type User_Account {
-    id: Number
+    id: Int
     username: String
     password: String
     firstName: String
@@ -16,46 +16,46 @@ const typeDefs = gql`
     projectManager: Boolean
   }
 
-  const users = [
-  {
-    id: 1,
-    username: 'Diana',
-    password: 'IamWonderwomen',
-    firstName: 'Diana',
-    lastName: 'V',
-    projectManager: true,
-  },
-  {
-    id: 1,
-    username: 'Ghenadie',
-    password: 'IamBatman',
-    firstName: 'Ghenadie',
-    lastName: 'P',
-    projectManager: false,
-  },
-];
-
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     users: [User_Account]
   }
+`;
 
-  # Resolvers define the technique for fetching the types defined in the
-  #schema. This resolver retrieves books from the "books" array above.
+const users = [
+    {
+      id: 1,
+      username: 'Diana',
+      password: 'IamWonderwomen',
+      firstName: 'Diana',
+      lastName: 'V',
+      projectManager: true,
+    },
+    {
+      id: 1,
+      username: 'Ghenadie',
+      password: 'IamBatman',
+      firstName: 'Ghenadie',
+      lastName: 'P',
+      projectManager: false,
+    },
+  ];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-  Query: {
-    users: () => users,
-  },
+Query: {
+  users: () => users,
+},
 };
 
-# The ApolloServer constructor requires two parameters: your schema
-# definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers, uploads: false, });
 
-# The listen method launches a web server.
+// The listen method launches a web server.
 server.listen().then(({ url }) => {
-  console.log("🚀  Server ready at ${url}");
+    console.log(`🚀  Server ready at ${url}`);
 });
-`;
