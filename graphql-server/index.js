1e8271a36f44c0262dcd7879da2e65d00a6f2f9f
Diana <diana@Puscas.local>
2020-05-07 20:23:37 +0300

    Add apollo server

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -49,4 +49,13 @@ const resolvers = {
     users: () => users,
   },
 };
+
+# The ApolloServer constructor requires two parameters: your schema
+# definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+# The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
 `;
