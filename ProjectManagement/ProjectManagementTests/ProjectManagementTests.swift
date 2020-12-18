3703541918566836f07655c27c94d5ff9ae14880
Diana Vasiliev <dianaanca94@gmail.com>
2020-12-18 19:18:23 +0200

    Integrate Apollo framework
    Integrate as a Swift packa

---
+++ ProjectManagement/ProjectManagementTests/ProjectManagementTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectManagementTests.swift
+//  ProjectManagementTests
+//
+//  Created by Diana on 17/12/2020.
+//  Copyright © 2020 Diana. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectManagement
+
+class ProjectManagementTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
