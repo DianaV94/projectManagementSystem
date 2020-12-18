d5197c4dcea411774d50a28c11d5dd46166b501f
Diana Vasiliev <dianaanca94@gmail.com>
2020-12-18 19:54:28 +0200

    Integrated Codegen script in main project
    - Integrated the script in main project in build phase
      so the code is generated each time main project builds.

--- ProjectManagementMacOS/Codegen/Sources/Codegen/main.swift
+++ ProjectManagementMacOS/Codegen/Sources/Codegen/main.swift
@@ -30,6 +30,17 @@ do {
   exit(1)
 }
 
+let targetURL = sourceRootURL
+    .apollo.childFolderURL(folderName: "ProjectManagementMacOS")
+let codegenOptions = ApolloCodegenOptions(targetRootURL: targetURL)
+
+do {
+    try ApolloCodegen.run(from: targetURL,
+                          with: cliFolderURL,
+                          options: codegenOptions)
+} catch {
+    exit(1)
+}
 
 
 
