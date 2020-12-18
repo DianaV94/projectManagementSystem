c8207d75c8ad46cb5ac5eb5ee1a0e654a3302a6c
Diana Vasiliev <dianaanca94@gmail.com>
2020-12-18 19:56:50 +0200

    Small code refactoring

--- ProjectManagementMacOS/Codegen/Sources/Codegen/main.swift
+++ ProjectManagementMacOS/Codegen/Sources/Codegen/main.swift
@@ -1,48 +1,57 @@
-import ApolloCodegenLib
 import Foundation
+import ApolloCodegenLib
 
+// Grab the parent folder of this file on the filesystem
 let parentFolderOfScriptFile = FileFinder.findParentFolder()
+
+// Use that to calculate the source root of both the
 let sourceRootURL = parentFolderOfScriptFile
-  .apollo.parentFolderURL() // Result: Sources folder
-  .apollo.parentFolderURL() // Result: Codegen folder
-  .apollo.parentFolderURL() // Result: MyProject source root folder
+    .apollo.parentFolderURL() // Sources
+    .apollo.parentFolderURL() // Codegen
+    .apollo.parentFolderURL() // MyProject
 
-let cliFolderURL = sourceRootURL
-  .apollo.childFolderURL(folderName: "Codegen")
-  .apollo.childFolderURL(folderName: "ApolloCLI")
+// From the source root, figure out where your target
+// root is within your main project
+let targetRootURL = sourceRootURL
+    .apollo.childFolderURL(folderName: "ProjectManagementMacOS")
 
-let output = sourceRootURL
-    .apollo.childFolderURL(folderName:"ProjectManagementMacOS")
-
+// Set up the URL you want to use to download the project
 let endpoint = URL(string: "http://localhost:2003")!
 
-try FileManager
-  .default
-  .apollo.createFolderIfNeeded(at: output)
-
+// Create an options object for downloading the schema
 let schemaDownloadOptions = ApolloSchemaOptions(endpointURL: endpoint,
-                                                outputFolderURL: output)
+                                                outputFolderURL: targetRootURL)
+                                      
+// Calculate where you want to create the folder where the CLI will
+// be downloaded by the ApolloCodegenLib framework.
+let cliFolderURL = sourceRootURL
+    .apollo.childFolderURL(folderName: "Codegen")
+    .apollo.childFolderURL(folderName: "ApolloCLI")
 
 do {
+  // Actually attempt to download the schema.
   try ApolloSchemaDownloader.run(with: cliFolderURL,
                                  options: schemaDownloadOptions)
 } catch {
+  // This makes the error message in Xcode a lot more legible,
+  // and prevents the script from continuing to try to generate
+  // code if the schema download failed.
   exit(1)
 }
 
-let targetURL = sourceRootURL
-    .apollo.childFolderURL(folderName: "ProjectManagementMacOS")
-let codegenOptions = ApolloCodegenOptions(targetRootURL: targetURL)
+// Create the default Codegen options object (assumes schema.json
+// is in the target root folder, all queries are in some kind
+// of subfolder of the target folder and will output as a
+// single file to API.swift in the target folder)
+let codegenOptions = ApolloCodegenOptions(targetRootURL: targetRootURL)
 
 do {
-    try ApolloCodegen.run(from: targetURL,
+    // Actually attempt to generate code.
+    try ApolloCodegen.run(from: targetRootURL,
                           with: cliFolderURL,
                           options: codegenOptions)
 } catch {
+    // This makes the error message in Xcode a lot more legible.
     exit(1)
 }
 
-
-
-
-print(sourceRootURL)
